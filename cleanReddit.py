46b773fbf8f424b8d2ca8a9fe2a8b9ca959f0fca
dharmanshu24 <36137804+dharmanshu24@users.noreply.github.com>
2018-02-16 15:41:58 +0530

    Add files via upload

---
+++ cleanReddit.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3
+import praw
+import time
+
+
+def removeComments(username, reddit, requests):
+    for comment in reddit.redditor(username).comments.new(limit=None):
+        if(requests >= 998):
+            print("30s break or else Reddit gods will get angry")
+            time.sleep(30)
+        print(comment.body.split('\n', 1)[0:79])
+        comment.edit('Does this even work?')
+        comment.delete()
+        requests += 1
+    return requests
+
+
+def removePosts(username, reddit, requests):
+    for submission in reddit.redditor(username).submissions.top('all'):
+        if(requests >= 998):
+            print("30s break or else Reddit gods will get angry")
+            time.sleep(30)
+        print(submission.title)
+        submission.delete()
+        requests += 1
+    return requests
+
+
+client_id = input("client_id=")
+client_secret = input("client_secret=")
+password = input("password=")
+username = input("username=")
+requests = 0
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, password=password, user_agent=username, username=username)
+
+print('Removing Posts')
+requests += removePosts(username, reddit, requests)
+
+print('Removing Comments')
+requests += removeComments(username, reddit, requests)
